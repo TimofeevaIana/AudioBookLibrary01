e51ca9ede7374685bad2786ef5ae33fb2d221e28
TimofeevaIana <70201424@online.muiv.ru>
2025-05-23 14:47:12 +0300

    Настройка взаимодействия с сервером через Retrofit

--- app/src/main/java/com/example/audiobooklibrary/network/RetrofitClient.kt
+++ app/src/main/java/com/example/audiobooklibrary/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://your-server-url:port/" // Replace with your server URL
+    private const val BASE_URL = "http://10.0.2.2:3000/" // URL для Android-эмулятора
 
     val api: AudioBookApi by lazy {
         Retrofit.Builder()
